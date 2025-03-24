10d8e0763970e49c091907430736da37784312ce
Emil Pedersen <steamman0103@gmail.com>
2025-03-24 15:39:46 +0100

    update

--- emil.py
+++ emil.py
@@ -2,25 +2,46 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, Motor
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,OUTPUT_D, SpeedPercent, MoveTank, Motor,MoveSteering
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
 
 
-input("press to start")
+# input("press to start")
 
-print("driving")
-
-
-#Get the motor to driving part
-
-m = Motor(OUTPUT_A)
-# n = LargeMotor(OUTPUT_B)
-m.on_for_rotations(-100, 25)
-
-# m.on_for_rotations(100, 3)
-# n.on_for_rotations(-100, 5)
+# print("driving")
 
 
-print("done")
+# #Get the motor to driving part
+
+# m = Motor(OUTPUT_A)
+# # n = LargeMotor(OUTPUT_B)
+# m.on_for_rotations(-100, 25)
+
+# # m.on_for_rotations(100, 3)
+# # n.on_for_rotations(-100, 5)
+
+
+# print("done")
+
+
+# LargeMotor(address=OUTPUT_A).on(speed=0)
+
+robot = MoveSteering(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_B)
+# grabber = LargeMotor(address=OUTPUT_C)
+
+# grabber.on(speed=100)
+hej = ""
+while hej != 'q':
+    hej = input("type")
+    if(hej == 'q'):
+        robot.off()
+        grabber.off()
+    if(hej == 'g'):
+        GrabberHelper = Motor(OUTPUT_D)
+        GrabberHelper.on_for_rotations(speed=-100, rotations=10)
+        GrabberHelper.on_for_rotations(speed=100, rotations=10)
+
+    else:
+        robot.on(steering=0,speed=int(hej))
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from math import pi
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("192.168.138.143", 12353))
+server_socket.bind(("192.168.138.130", 12358))
 server_socket.listen(1)
 
 print("Waiting for connection...")
@@ -17,13 +17,15 @@ conn, addr = server_socket.accept()
 
 userinputforMetoh = ''
 
-from ev3dev2.motor import LargeMotor, Motor, OUTPUT_A, OUTPUT_B, OUTPUT_C, SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, Motor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MoveSteering
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor, GyroSensor
 from ev3dev2.led import Leds
 
-
 # userinput = input("Press enter to start program")
+robot = MoveSteering(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_B)
+# grabber = LargeMotor(address=OUTPUT_C)
+
 
 
 while(userinputforMetoh != 'q'):
@@ -32,70 +34,38 @@ while(userinputforMetoh != 'q'):
     wheel_circumf = 25*pi*2
     Robotwith = 220
 
-
     while userinputforMetoh == "":
-        print("wait for input")
         userinputforMetoh= conn.recv(1024).decode()
+        # userinputforMetoh.split("\n", -1)[0]
+        #print("wait for input")
+        #print(userinputforMetoh)
+        #print("\n")
+        splitInput = userinputforMetoh.split("\n")[-2].split("#") 
+        Rotate = int(splitInput[0])
         
-        Rotate = userinputforMetoh.split("#",1)[0]
-        
-        moment = userinputforMetoh.split("#",1)[-1]
+        moment = int(splitInput[1])
 
-        print("Received: " + userinputforMetoh)
+        #print("Received: " + userinputforMetoh)
+        print("moment: ", Rotate)
+        print("Rotate: ", moment)
         if userinputforMetoh == 'q':
+            # grabber.off()
+            robot.off()
             conn.close()
             server_socket.close()
             exit()
-
-    print("Initializing")
-    mAll = MoveTank(OUTPUT_A, OUTPUT_B, motor_class=Motor)
-    
-    def movemm(mm):
-        """
-        @parm The length that is need to drive forward.   
-        """
-        print("Driving motors")
-        print("circ: ", wheel_circumf)
-        rotations = mm/wheel_circumf
-        print("rot:", rotations)
-        mAll.on_for_rotations(left_speed=SpeedPercent(50), right_speed=SpeedPercent(50), rotations=rotations) 
-
-    
-    def Rotatefunc(Deg):
-        """
-        @parm The length that is need to drive forward.   
-        """
-        mm = Deg/Robotwith
-        print("Driving motors")
-        print("circ: ", wheel_circumf)
-        rotations = (mm/wheel_circumf) * 100
-        print("rot:", rotations)
-        mAll.on_for_rotations(left_speed=SpeedPercent(50), right_speed=SpeedPercent(-50), rotations=rotations) 
-
-    
-    def GrabberHelper():
-        """
-        The helper grabber for the main grabber if a bold is in the coner.
-        """
-        GrabberHelper = Motor(OUTPUT_C)
-        GrabberHelper.on_for_rotations(speed=-100, rotations=10)
-        GrabberHelper.on_for_rotations(speed=100, rotations=10)
+        
+    # def GrabberHelper():
+    #     """
+    #     The helper grabber for the main grabber if a bold is in the coner.
+    #     """
+    #     GrabberHelper = Motor(OUTPUT_C)
+    #     GrabberHelper.on_for_rotations(speed=-100, rotations=10)
+    #     GrabberHelper.on_for_rotations(speed=100, rotations=10)
     
     
-    def mainGrabber():
-        """
-        Spinning the main grabber.
-        """
-        pass
+    # grabber.on(speed=100)
+
     
 
-    def Dropping():
-        """
-        Reversing the main grabber so all the ball will fall out.
-        """
-        pass
-
-
-    mAll.on()
-    movemm(int(moment))
-    Rotatefunc(int(Rotate))
+    robot.on(steering=Rotate,speed=moment) 
